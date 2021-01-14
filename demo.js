164479bd106960dbf985c2081b131a1801ed8c85
cacaaotesouro <77461090+cacaaotesouro@users.noreply.github.com>
2021-01-14 16:46:25 -0300

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,10 +4,10 @@ var a=document.getElementById("text_a").value;
 
 if (a=="hello")
   {
-document.getElementById("answer").innerText="Correct!";
+document.getElementById("answer").innerText="Acertou! Agora sua próxima pista: O que é o que é XXX YYY ZZZ";
   }
 else
   {
-document.getElementById("answer").innerHTML="Oops!";
+document.getElementById("answer").innerHTML="Oops! Errou! Tente novamente!";
   }
 }
